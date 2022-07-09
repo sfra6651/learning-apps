64c8f24071a4b85c350c13ffdc5143c8bf95c2f9
Shaun <shaun@Shauns-Air.lan>
2022-07-09 13:10:58 +1200

    test

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ function App() {
   return (
     <div>
       <h1> My Todos</h1>
-      <Todo text="learn react" />
-      <Todo text="live react" />
-      <Todo text="be react" />
     </div>
   );
 }
