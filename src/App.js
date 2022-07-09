9e2c0bb507ee834235242c59dd0bba9adf810006
Shaun <shaun@Shauns-Air.lan>
2022-07-09 22:31:09 +1200

    layout added

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import AllMeetUps from "./pages/AllMeetUps";
 import Favourites from "./pages/Favourites";
 import NewMeetUps from "./pages/NewMeetUps";
 import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <BrowserRouter>
-      <MainNavigation />
-      <Routes>
-        <Route path="/" exact element={<AllMeetUps />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/favourites" element={<Favourites />}></Route>
-        <Route path="/newmeetups" element={<NewMeetUps />}></Route>
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" exact element={<AllMeetUps />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/favourites" element={<Favourites />}></Route>
+          <Route path="/newmeetups" element={<NewMeetUps />}></Route>
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,17 +1,22 @@
+import classes from "./MeetupItem.module.css";
+import Card from "../ui/Card";
+
 function MeetupItem(props) {
   return (
-    <li>
-      <div>
-        <img src={props.image} alts={props.title} />
-      </div>
-      <div>
-        <h3>{props.title}</h3>
-        <address>{props.address}</address>
-        <p>{props.description}</p>
-      </div>
-      <div>
-        <button>To Favourites</button>
-      </div>
+    <li className={classes.item}>
+      <Card>
+        <div className={classes.image}>
+          <img src={props.image} alts={props.title} />
+        </div>
+        <div className={classes.content}>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
+        </div>
+        <div className={classes.actions}>
+          <button>To Favourites</button>
+        </div>
+      </Card>
     </li>
   );
 }
