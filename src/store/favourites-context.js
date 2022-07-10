ae06168de5e2a88a256499c7e64c6d665495e246
Shaun <shaun@Shauns-Air.lan>
2022-07-10 17:38:06 +1200

    finished

---
+++ src/store/favourites-context.js
@@ -0,0 +1,47 @@
+import { createContext } from "react";
+import { useState } from "react";
+
+const FavouritesContext = createContext({
+  favourites: [],
+  totalFavourites: 0,
+  //these lines for beter auto completion later. doesnt actually do anything
+  addFavourite: (favouriteMeetup) => {},
+  removeFavourite: (meetupId) => {},
+  itemIsFavourite: (meetupId) => {},
+});
+
+export function FavouritesContextProvider(props) {
+  const [userFavourites, setUserFavouritses] = useState([]);
+
+  function addFavouriteHandler(favouriteMeetup) {
+    setUserFavouritses((prevUserFavourites) => {
+      return prevUserFavourites.concat(favouriteMeetup);
+    });
+  }
+
+  function removeFavouriteHandler(meetupId) {
+    setUserFavouritses((prevUserFavourites) => {
+      return prevUserFavourites.filter((meetup) => meetup.id !== meetupId);
+    });
+  }
+
+  function itemIsFavouriteHandler(meetupId) {
+    return userFavourites.some((meetup) => meetup.id === meetupId);
+  }
+
+  const context = {
+    favourites: userFavourites,
+    totalFavourites: userFavourites.length,
+    addFavourite: addFavouriteHandler,
+    removeFavourite: removeFavouriteHandler,
+    itemIsFavourite: itemIsFavouriteHandler,
+  };
+
+  return (
+    <FavouritesContext.Provider value={context}>
+      {props.children}
+    </FavouritesContext.Provider>
+  );
+}
+
+export default FavouritesContext;
